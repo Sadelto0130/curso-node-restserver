8a3cab9b237cb4ace2a3d4646fb870b6b83a519f
Victor <vsalaz00@live.com>
2022-05-30 23:49:45 -0300

    Validaciones middlewares completas

--- controllers/user.js
+++ controllers/user.js
@@ -1,6 +1,5 @@
 const bcryptjs = require('bcryptjs');
 const { response, request } = require('express');
-const { validationResult } = require('express-validator');
 const res = response;
 const req = request;
 
@@ -19,11 +18,6 @@ const userGet = (req, res) => {
 }
 
 const userPost = async(req, res) => {
-
-  const errors = validationResult(req);
-  if(!errors.isEmpty()){
-    return res.status(400).json(errors);
-  }
 
   const {nombre, correo, password, rol} = req.body;
   const usuario = new Usuario({nombre, correo, password, rol});
--- routes/user.js
+++ routes/user.js
@@ -1,18 +1,20 @@
 const { Router } = require('express');
 const { check } = require('express-validator');
 //const { userGet, userPost, userDelete, userPut } = require('../controllers/user');
-const controllersUser = require('../controllers/user')
+const controllersUser = require('../controllers/user');
+const { validarCampos } = require('../middlewares/validar-campos');
 
 const router = Router()
 
 router.get('/', controllersUser.userGet);
 
 router.post('/', [
-    check('nombre', 'El nombre es obligatorio').notEmpty(),
-    check('password', 'El password debe tener mas de 6 caracteres').isLength({min: 6}),
-    check('correo', 'El correo no es valido').isEmail(),
-    check('rol', 'No es un rol válido').isIn(['ADMIN', 'USER']),
-],controllersUser.userPost);
+  check('nombre', 'El nombre es obligatorio').notEmpty(),
+  check('password', 'El password debe tener mas de 6 caracteres').isLength({ min: 6 }),
+  check('correo', 'El correo no es valido').isEmail(),
+  check('rol', 'No es un rol válido').isIn(['ADMIN', 'USER']),
+  validarCampos
+], controllersUser.userPost);
 
 router.put('/:id', controllersUser.userPut);
 
